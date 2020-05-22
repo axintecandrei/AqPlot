34e0e06eb71128001770e4afce74d830c3059c3b
Rendai Exatin <axintecandrei@gmail.com>
2020-05-22 19:06:00 +0300

    small updates
    - app icon with .ico
    - update prepare_exe script with no console and icon

--- Src/prepare_exe.bat
+++ Src/prepare_exe.bat
@@ -1,2 +1,2 @@
-"c:\Users\uidp7521\AppData\Local\Programs\Python\Python36\Scripts\pyinstaller.exe" --name "AqPlot" --onefile  %1
+"c:\Users\uidp7521\AppData\Local\Programs\Python\Python36\Scripts\pyinstaller.exe" --name "AqPlot" --icon "D:\casdev\sbxs\github_com\AqPlot\Src\GUI\plot_icon.ico" --windowed --onefile  %1
 pause
